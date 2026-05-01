69657b9fcf9b344b47b2bcaeaa973e9e857171ad
Ya2yo <yayoy0015@gmail.com>
2026-05-01 19:06:22 +0800

    恢复之前arch调试进行的修改

--- os/src/arch/mod.rs
+++ os/src/arch/mod.rs
@@ -1,11 +1,9 @@
-// cfg_if::cfg_if! {
-//     if #[cfg(feature = "riscv64")] {
-//         mod riscv64;
-//         pub use riscv64::*;
-//     } else if #[cfg(feature = "loongarch64")] {
-//         mod loongarch64;
-//         pub use loongarch64::*;
-//     }
-// }
-mod riscv64;
-pub use riscv64::*;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "riscv64")] {
+        mod riscv64;
+        pub use riscv64::*;
+    } else if #[cfg(feature = "loongarch64")] {
+        mod loongarch64;
+        pub use loongarch64::*;
+    }
+}
