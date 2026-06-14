4621db741dd0c6e1289f2ce3350a06dd129f4082
Ya2yo <yayoy0015@gmail.com>
2026-06-14 12:45:40 +0800

    fix: 全ltp测试

--- user/src/bin/initproc.rs
+++ user/src/bin/initproc.rs
@@ -114,8 +114,8 @@ fn get_score() {
     // run_testsuit("musl\0", "libcbench_testcode.sh\0");// 龙芯 riscv 通过
     // run_testsuit("glibc\0", "libcbench_testcode.sh\0");// riscv loongarch 通过
     // ltp
-    // ltp::test_musl_ltp();
-    ltp::test_musl_single("abort01\0");
+    ltp::test_musl_ltp();
+    // ltp::test_musl_single("abort01\0");
     // ltp::test_glibc_ltp();
 
         // #[cfg(target_arch = "riscv64")]
