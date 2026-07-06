f481f2ba110d5eae4e99eb7b2c9008a24ee83ed9
Ya2yo <yayoy0015@gmail.com>
2026-07-06 16:16:53 +0800

    style: move chroot to fs

--- os/src/syscall/fs/ctl.rs
+++ os/src/syscall/fs/ctl.rs
@@ -750,3 +750,37 @@ pub fn sys_fchmodat(dirfd: isize, path: *const u8, mode: u32, flags: u32) -> Sys
     inode.inode.fmode_set(mode);
     Ok(0)
 }
+
+/// 参考 https://man7.org/linux/man-pages/man2/chroot.2.html
+pub fn sys_chroot(path: *const u8) -> SyscallRet {
+    debug!("[chroot] path=0x{:x}", path as usize);
+
+    let task = current_task().unwrap();
+
+    if path.is_null() {
+        return Err(SysErrNo::EINVAL);
+    }
+    if (path as isize) <= 0 || if_bad_address(path as usize) {
+        return Err(SysErrNo::EFAULT);
+    }
+
+    if task.inner_lock().user_id != 0 {
+        return Err(SysErrNo::EPERM);
+    }
+
+    let path_str = {
+        let proc_inner = &task.process;
+        let memory_set = proc_inner.memory_set_arc();
+        read_user_cstr(&memory_set, path)?
+    };
+
+    let file = open(&path_str, OpenFlags::O_RDONLY, NONE_MODE)?;
+    let osfile = file.file()?;
+    if osfile.inode.types() != InodeType::Dir {
+        return Err(SysErrNo::ENOTDIR);
+    }
+
+    task.process.fs_info.set_cwd(path_str);
+    debug!("[chroot] success");
+    Ok(0)
+}
--- os/src/syscall/sys/mod.rs
+++ os/src/syscall/sys/mod.rs
@@ -1,9 +1,7 @@
 mod capability;
-mod fs;
 mod prctl;
 mod system;
 
 pub use capability::*;
-pub use fs::*;
 pub use prctl::*;
 pub use system::*;
